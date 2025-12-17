b59e585750b0f563ceb7a70ba786f478be02a942
Alexandra123713 <alexandracerevatii@gmail.com>
2025-12-17 18:27:26 +0200

    Fix Netlify routing base path

--- scripts/url-router.js
+++ scripts/url-router.js
@@ -4,7 +4,7 @@ import { initTypePage } from './type.js';
 import { initGamePage } from './script.js';
 
 const urlPageTitle = 'Quiz Game';
-const BASE_PATH = '/quiz-game';
+const BASE_PATH = '';
 
 document.addEventListener('click', (event) => {
 	const anchor = event.target.closest('a');
@@ -15,39 +15,39 @@ document.addEventListener('click', (event) => {
 
 const urlRoutes = {
 	404: {
-		template: './pages/404.html',
+		template: '/pages/404.html',
 		title: '404 | ' + urlPageTitle,
 		description: 'Page not found',
 	},
 	[`${BASE_PATH}/`]: {
-		template: './pages/home.html',
+		template: '/pages/home.html',
 		title: 'Home | ' + urlPageTitle,
 		description: 'This is the homepage',
 	},
 	[`${BASE_PATH}/categories`]: {
-		template: './pages/categories.html',
+		template: '/pages/categories.html',
 		title: 'Categories | ' + urlPageTitle,
 		description: 'This is the categories page',
 	},
 	[`${BASE_PATH}/difficulty`]: {
-		template: './pages/difficulty.html',
+		template: '/pages/difficulty.html',
 		title: 'Difficulty | ' + urlPageTitle,
 		description: 'This is the difficulty page',
 	},
 	[`${BASE_PATH}/type`]: {
-		template: './pages/type.html',
+		template: '/pages/type.html',
 		title: 'Type | ' + urlPageTitle,
 		description: 'This is the type page',
 	},
 	[`${BASE_PATH}/game`]: {
-		template: './pages/game.html',
+		template: '/pages/game.html',
 		title: 'Game | ' + urlPageTitle,
 		description: 'This is the game page',
 	},
 };
 
 const urlRoute = (url) => {
-	window.history.pushState({}, '', `${BASE_PATH}${url}`);
+	window.history.pushState({}, '', url);
 	urlLocationHandler();
 };
 
